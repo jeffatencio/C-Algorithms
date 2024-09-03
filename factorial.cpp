50ac58b6a5f2ef5923a0001261136da71ae478fc
unknown <jeffatencio123@gmail.com>
2024-09-03 19:14:13 -0400

    add factorial

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    
+    return factorial(n-1) * n;
+}
+
+int main(){
+
+    int result = factorial(5);
+    printf("%d", result);
+
+    return 0;
+}
